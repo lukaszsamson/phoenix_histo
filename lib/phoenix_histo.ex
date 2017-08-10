8039b7d7ec54a18ec9b1106f8ef2b9dcbc9574b5
Lukasz Samson <lukaszsamson@gmail.com>
2017-08-11 00:11:47 +0200

    Initial commit

---
+++ lib/phoenix_histo.ex
@@ -0,0 +1,18 @@
+defmodule PhoenixHisto do
+  @moduledoc """
+  Documentation for PhoenixHisto.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PhoenixHisto.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/phoenix_histo_test.exs
@@ -0,0 +1,8 @@
+defmodule PhoenixHistoTest do
+  use ExUnit.Case
+  doctest PhoenixHisto
+
+  test "greets the world" do
+    assert PhoenixHisto.hello() == :world
+  end
+end
